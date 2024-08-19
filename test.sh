b1a9e5c6538f22efc2b4cffc1193d190e856128d
echoRedOx <echodevs@proton.me>
2024-08-19 10:19:50 -0500

    updated test script

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ echo "Starting..."
 pyenv versions | grep -q 'ollama' && echo "ollamaui virtualenv installed" || pyenv virtualenv 3.11 ollama
 
 # Activate the virtual environment
+pyenv activate ollama
 
-
-# Confirm the environment is activated
+# Confirm  python version
+python --version
